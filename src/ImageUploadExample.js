112f942730ae4b19c3a6d47ccd58e0b21a928861
Giuseppe Messina <mariobros.gm@gmail.com>
2022-02-18 11:59:01 +0100

    finally the uploading of the image to strapi

--- src/ImageUploadExample.js
+++ src/ImageUploadExample.js
@@ -1,15 +1,19 @@
 import {Component} from "react";
-import {Container, Input} from "@mui/material";
-import {Row} from "react-bootstrap";
+import {Backdrop, CircularProgress, Container, Input} from "@mui/material";
+import {Col, Row} from "react-bootstrap";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import axios from "axios";
 
 class ImageUploadExample extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            picture: null
+            token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMmUzMmZkYzUxNWJkMDAzMTM0YWFjYSIsImlhdCI6MTY0NTE3NTIyNywiZXhwIjoxNjQ1MjYxNjI3LCJpc3MiOiJzdHJhcGkifQ.ulULRtOFmPGnjzqGtGoLu1s5bETL2T_8lIgokUhOPCI',
+            picture: null,
+            rawPicture: null,
+            downloadedPicture: null,
+            loading: false
         };
     }
 
@@ -27,45 +31,85 @@ class ImageUploadExample extends Component {
 
     onChangePicture = (e) => {
         this.setState({picture: URL.createObjectURL(e.target.files[0])});
+        this.setState({rawPicture: e.target.files[0]})
     };
 
     uploadImage = () => {
-        const myToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMmUzMmZkYzUxNWJkMDAzMTM0YWFjYSIsImlhdCI6MTY0NTE3NTIyNywiZXhwIjoxNjQ1MjYxNjI3LCJpc3MiOiJzdHJhcGkifQ.ulULRtOFmPGnjzqGtGoLu1s5bETL2T_8lIgokUhOPCI';
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + myToken,
-            },
-            body: JSON.stringify({ title: 'React POST Request Example' })
+        this.setLoading();
+        const formData = new FormData();
+        const data = {
+            email: 'ok6@ok6.it',
         };
-        fetch('http://zion.datafactor.it:40505/image-uploadeds', requestOptions)
-            .then(response => response.json())
-            .then(data => this.setState({ postId: data.id }));
+        formData.append('files.image', this.state.rawPicture, 'kurisu.jpg');
+        formData.append('data', JSON.stringify(data));
+        axios.post("http://zion.datafactor.it:40505/image-uploadeds", formData, {headers: {
+                'Authorization': 'Bearer ' + this.state.token,
+            }})
+            .then((response) => {
+                this.setIdle();
+                this.getImages();
+            }).catch((error) => {})
     }
+
+    getImages = () => {
+        this.setLoading();
+        axios.get("http://zion.datafactor.it:40505/image-uploadeds", {headers: {
+                'Authorization': 'Bearer ' + this.state.token,
+            }})
+            .then((response) => {
+                this.setState({downloadedPicture: "http://zion.datafactor.it:40505" + response.data[0].image.url})
+                this.setIdle();
+            }).catch((error) => {})
+    }
+
+    setLoading = () => {
+        this.setState({loading: true});
+    };
+
+    setIdle = () => {
+        this.setState({loading: false});
+    };
 
     render() {
         return (
             <Container>
+                <Backdrop
+                    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                    open={this.state.loading}>
+                    <CircularProgress color="inherit" />
+                </Backdrop>
                 <br/>
                 <br/>
                 <br/>
                 <br/>
-                <Row className="justify-content-center">
+                <Row className="justify-content-md-center">
                     <Typography variant='h2' className='text-center'>Prova upload immagine</Typography>
                 </Row>
                 <br/>
-                <Row className="justify-content-center">
-                    <label htmlFor="contained-button-file" className='text-center'>
-                        <Input accept="image/*" id="contained-button-file" type="file" hidden onChange={this.onChangePicture.bind(this)}/>
-                        <Button variant="contained" component="span">
-                            CARICA IMMAGINE
+                <Row className="justify-content-md-center">
+                    <Col className='text-center'>
+                        <label htmlFor="contained-button-file" className='text-center'>
+                            <Input accept="image/*" id="contained-button-file" type="file" hidden onChange={this.onChangePicture.bind(this)}/>
+                            <Button variant="contained" component="span">
+                                SCEGLI IMMAGINE
+                            </Button>
+                        </label>
+                    </Col>
+                    <Col className='text-center'>
+                        <Button variant="contained" disabled={this.state.picture == null} onClick={this.uploadImage.bind(this)}>
+                            UPLOAD IMMAGINE
                         </Button>
-                    </label>
+                    </Col>
                 </Row>
                 <br/>
                 <Row className="justify-content-center">
                     <img  src={this.state.picture}/>
+                </Row>
+                <br/>
+                <hr/>
+                <br/>
+                <Row className="justify-content-center">
+                    <img  src={this.state.downloadedPicture}/>
                 </Row>
             </Container>
         )
